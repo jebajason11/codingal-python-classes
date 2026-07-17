093a6bd35873b605c6b64afbbb4c7831a7ecf727
jebajason11 <jebajason11@gmail.com>
2026-07-17 17:22:12 -0500

    Updated project

---
+++ Moudule One lessone 4/Average.py
@@ -0,0 +1,11 @@
+tree1= 45
+tree2= 76
+tree3= 56
+tree4= 99
+tree5= 105
+
+sum=(tree1+tree2+tree3+tree4+tree5)
+print("sum of all five tree is",sum)
+
+average=sum/5
+print("average of all five trees", average)
