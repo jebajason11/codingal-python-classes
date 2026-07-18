26b8f420ecf7dd07af856b2f5af4b28b02a4ca19
jebajason11 <jebajason11@gmail.com>
2026-07-17 22:20:44 -0500

    Updated project

--- Moudule One lessone 4/Count the notes.py
+++ Moudule One lessone 4/Count the notes.py
@@ -4,4 +4,4 @@ note2=(Amount%100)//50
 note3=((Amount%100)%50)//10
 print("notes of 100 rupee", note1)
 print("notes of 50 rupee", note2)
-print("notes of 10 rupee", note3)14555555550
+print("notes of 10 rupee", note3)
---
+++ Moudule One lessone 4/Percentage caluculations.py
@@ -0,0 +1,10 @@
+print("Enter Marks in 4 Subjects:")
+math=int(input("maths:"))
+english=int(input("english:"))
+science=int(input("science:"))
+hindi=int(input("hindi:"))
+sum=math+english+science+hindi
+print("sum of math,english,science and hindi=",sum)
+perc=(sum/400)*100
+print(end="Percentage Mark=")
+print(perc)
