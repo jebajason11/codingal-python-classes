342419008496d97185df90e19786875bab2f20cc
jebajason11 <jebajason11@gmail.com>
2026-07-18 15:30:19 -0500

    Updated project

---
+++ Module 1 lessson 5/Checking the number greater or smaller/Checking the number greater or smaller.py
@@ -0,0 +1,8 @@
+i = int(input("enter a number: "))
+if (i<15):
+    print ("i is smaller than 15")
+    print ("I am in if Block")
+else:
+    print ("i is greater than 15")
+    print ("I am in else Block")
+print ("I am not in if and not in else Block")
