98601dfedd47ef8fe4c1b9fee689f1c8fde4e611
jebajason11 <jebajason11@gmail.com>
2026-08-04 11:45:47 -0500

    Updated project

---
+++ Module 1 lessson 5/Condintional statement.py
@@ -0,0 +1,8 @@
+num=5
+if num > 0:
+    print(num, "is positive")
+
+
+num=-17
+if num > 0:
+    print(num,"is positive")
---
+++ Moudule 1 lesson 6/and or operater.py
@@ -0,0 +1,27 @@
+a = 13
+b = -4
+c = 21
+
+
+if a and b and c:
+    print("All true")
+
+else:
+    print("at least one false")
+
+a = 5
+b = -5
+c = 14
+
+if a > 0 or b > 0:
+    print("either is greater than 0")
+
+else:
+    print("no number is greater than 0")
+
+
+if b > 0 or c > 0:
+    print("either is greater than O")
+
+else:
+    print("no numer is greater than 0")
