2c13ff42c30bc80cc37af49584c8db47fd0a0699
jebajason11 <jebajason11@gmail.com>
2026-08-06 11:57:00 -0500

    Updated project

---
+++ Moudule 2 lesson 2/Reverse string.py
@@ -0,0 +1,9 @@
+string = input ("Enter your string :")
+
+string2 = (" ")
+
+for i in string:
+    string2 = i + string2
+
+print("\nThe Original String =", string)
+print("The reversed string =", string2)
