2aa4e988de792ab772c67963a3ae2a9c23e0da91
jebajason11 <jebajason11@gmail.com>
2026-08-06 10:32:35 -0500

    Updated project

---
+++ Moudule 1 lesson 6/Appilcation of logical not operator in python.py
@@ -0,0 +1,24 @@
+a = 12
+b = 15
+c = 43
+
+print(not(a == b))
+
+print(not(b == c))
+
+a="python"
+b="coding"
+
+if not (a == b):
+    print(a, "and",b,"are different")
+
+a=4
+b=5
+
+if not((a == 1) == (b == 5)):
+    print("Hello")
+
+a = int(input("Enter a number"))
+
+if not (a % 2 == 0):
+    print("a,is a odd number")
