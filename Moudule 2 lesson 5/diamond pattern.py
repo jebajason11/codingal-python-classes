8dd8cd3d991d62235614e1c19c6844c9c27bdc8f
jebajason11 <jebajason11@gmail.com>
2026-08-19 12:57:45 -0500

    Updated project

---
+++ Moudule 2 lesson 5/diamond pattern.py
@@ -0,0 +1,13 @@
+n = int(input("enter the number of rows:"))
+
+for i in range(1, n+ 1):
+    print(" "* (n-i), end="")
+    for j in range(1,i * 2):
+        print(j,end="")
+    print()
+
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i), end="")
+    for j in range(1, i * 2):
+        print(j, end="")
+    print()
